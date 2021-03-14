b0fd3ed221750f895f1cd6a7692d5ed5eaee5237
ogu0620 <ogu.smiley.6_20@me.com>
2021-03-14 13:14:11 +0900

    商品削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  resources :items, only: [:new, :create,:show,:edit,:update]
+  resources :items, only: [:new, :create,:show,:edit,:update,:destroy]
 
 root to: "items#index"
 end
